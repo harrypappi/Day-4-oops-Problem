4ee5b047f5bd6a87022e2a1fe63a88a8d91d8fb1
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-28 12:29:06 +0530

    First

---
+++ UC1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Employee
+{
+    public string Name { get; set; }
+
+    public bool IsPresent()
+    {
+        Random random = new Random();
+        return random.Next(2) == 0;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Employee employee = new Employee();
+        employee.Name = "Harishkumar";
+
+        bool isPresent = employee.IsPresent();
+
+        if (isPresent)
+        {
+            Console.WriteLine($"{employee.Name} is present.");
+        }
+        else
+        {
+            Console.WriteLine($"{employee.Name} is absent.");
+        }
+    }
+}
