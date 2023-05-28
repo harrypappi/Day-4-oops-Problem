a5aa33cf43fe2087a1bae25a3fa427edfa1e58c6
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-28 12:30:42 +0530

    secound

---
+++ UC2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int WagePerHour { get; set; }
+    public int FullDayHour { get; set; }
+
+    public int CalculateDailyWage()
+    {
+        return WagePerHour * FullDayHour;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Employee employee = new Employee();
+        employee.Name = "Harishkumar";
+        employee.WagePerHour = 20;
+        employee.FullDayHour = 8;
+
+        int dailyWage = employee.CalculateDailyWage();
+
+        Console.WriteLine($"Daily wage of {employee.Name} is: {dailyWage}");
+    }
+}
