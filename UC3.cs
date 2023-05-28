e1388de3f6c8eb1bf4eeb2688e0b8bf62c259ab6
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-28 12:33:12 +0530

    Third Fourth

---
+++ UC3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int WagePerHour { get; set; }
+    public int FullDayHour { get; set; }
+    public int PartTimeHour { get; set; }
+
+    public int CalculateDailyWage(bool isFullTime)
+    {
+        if (isFullTime)
+        {
+            return WagePerHour * FullDayHour;
+        }
+        else
+        {
+            return WagePerHour * PartTimeHour;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Employee fullTimeEmployee = new Employee();
+        fullTimeEmployee.Name = "Harry";
+        fullTimeEmployee.WagePerHour = 20;
+        fullTimeEmployee.FullDayHour = 8;
+        fullTimeEmployee.PartTimeHour = 8;
+
+        int fullTimeWage = fullTimeEmployee.CalculateDailyWage(true);
+
+        Console.WriteLine($"Daily wage of {fullTimeEmployee.Name} (Full Time) is: {fullTimeWage}");
+
+        Employee partTimeEmployee = new Employee();
+        partTimeEmployee.Name = "pappi";
+        partTimeEmployee.WagePerHour = 15;
+        partTimeEmployee.FullDayHour = 8;
+        partTimeEmployee.PartTimeHour = 8;
+
+        int partTimeWage = partTimeEmployee.CalculateDailyWage(false);
+
+        Console.WriteLine($"Daily wage of {partTimeEmployee.Name} (Part Time) is: {partTimeWage}");
+    }
+}
---
+++ UC4.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int WagePerHour { get; set; }
+    public int FullDayHour { get; set; }
+    public int PartTimeHour { get; set; }
+
+    public int CalculateDailyWage(string employeeType)
+    {
+        int dailyWage = 0;
+
+        switch (employeeType)
+        {
+            case "FullTime":
+                dailyWage = WagePerHour * FullDayHour;
+                break;
+            case "PartTime":
+                dailyWage = WagePerHour * PartTimeHour;
+                break;
+            default:
+                Console.WriteLine("Invalid employee type.");
+                break;
+        }
+
+        return dailyWage;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Employee employee = new Employee();
+        employee.Name = "Harishkumar";
+        employee.WagePerHour = 20;
+        employee.FullDayHour = 8;
+        employee.PartTimeHour = 8;
+
+        string employeeType = "FullTime";
+        int dailyWage = employee.CalculateDailyWage(employeeType);
+
+        Console.WriteLine($"Daily wage of {employee.Name} (Full Time) is: {dailyWage}");
+
+        employeeType = "PartTime";
+        dailyWage = employee.CalculateDailyWage(employeeType);
+
+        Console.WriteLine($"Daily wage of {employee.Name} (Part Time) is: {dailyWage}");
+    }
+}
